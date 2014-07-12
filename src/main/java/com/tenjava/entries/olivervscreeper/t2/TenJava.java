93e63be4ca5953c0168134b1271da395898f796a
OliverVsCreeper <dev@olivervscreeper.co.uk>
2014-07-12 15:29:24 +0100

    First Commit

--- src/main/java/com/tenjava/entries/olivervscreeper/t2/TenJava.java
+++ src/main/java/com/tenjava/entries/olivervscreeper/t2/TenJava.java
@@ -1,5 +1,23 @@
 package com.tenjava.entries.olivervscreeper.t2;
 
+import com.tenjava.entries.olivervscreeper.t2.listeners.PlayerListener;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class TenJava extends JavaPlugin {}
+public class TenJava extends JavaPlugin {
+
+    public static Plugin plugin;
+
+    public void onEnable(){
+        this.getLogger().info("Ore-a Enabled! Performing setup..."); //Begin
+        init();
+    }
+
+    public void init(){
+        plugin = this; //Save plugin for other classes.
+        Bukkit.getPluginManager()
+                .registerEvents(new PlayerListener(),this); //Register listener
+    }
+
+}
---
+++ src/main/java/com/tenjava/entries/olivervscreeper/t2/listeners/PlayerListener.java
@@ -0,0 +1,19 @@
+package com.tenjava.entries.olivervscreeper.t2.listeners;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+/**
+ * Created on 12/07/2014.
+ *
+ * @author OliverVsCreeper
+ */
+public class PlayerListener implements Listener{
+
+    @EventHandler //Event to award energy.
+    public void onBreak(BlockBreakEvent e){
+
+    }
+
+}
